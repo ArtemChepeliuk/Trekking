57c5f6d20d73881d73362578117bd6cab4a60806
ArtemChepeliuk <4epeliuk@gmail.com>
2018-07-13 17:55:00 +0300

    first commit

---
+++ js/main.js
@@ -0,0 +1,24 @@
+var slideIndex = 1;
+showDivs(slideIndex);
+
+function plusDivsMen(n) {
+    showDivs(slideIndex += n);
+}
+
+function showDivs(n) {
+    var i;
+    var x = document.getElementsByClassName("slider_men");
+    if (n > x.length) {slideIndex = 1} 
+    if (n < 1) {slideIndex = x.length} ;
+    for (i = 0; i < x.length; i++) {
+        x[i].style.display = "none"; 
+    }
+    x[slideIndex-1].style.display = "flex"; 
+}
+
+
+$(document).ready(function() {
+    $('.carousel').carousel({
+      interval: 6000
+    })
+  });
