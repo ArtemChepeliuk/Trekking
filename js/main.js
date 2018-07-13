07b04687959aec47aa5744b283293d590ede93b9
ArtemChepeliuk <4epeliuk@gmail.com>
2018-07-13 23:22:31 +0300

    add checkbox

--- js/main.js
+++ js/main.js
@@ -1,8 +1,6 @@
 var slideIndex = 1;
 showDivs(slideIndex);
 
-
-
 function plusDivsMen(n) {
     showDivs(slideIndex += n);
 }
@@ -21,4 +19,18 @@ function showDivs(n) {
     }
     x[slideIndex - 1].style.display = "flex";
     document.getElementById('number').innerHTML = slideIndex;
+}
+
+
+
+
+function showOrHide(Men, sectionTwo) {
+    var checkbox = document.getElementById('Men');
+    var section = document.getElementById("sectionTwo");
+    if (checkbox.checked) {
+        section.style.display = "block";
+    } else {
+        section.style.display = "none";
+
+    }
 }
