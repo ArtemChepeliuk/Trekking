d542bd07e2801ffde9c3839ef64064cfe8ee7db0
ArtemChepeliuk <4epeliuk@gmail.com>
2018-07-17 00:10:36 +0300

    done with desktop

--- js/main.js
+++ js/main.js
@@ -1,13 +1,13 @@
 var slideIndex = 1;
-showDivsMen(slideIndex);
+showDivs(slideIndex);
 
-function plusDivsMen(n) {
-    showDivsMen(slideIndex += n);
-
+function plusDivs(n, slider, number) {
+    showDivs(slideIndex += n, slider, number);
 }
 
-function showDivsMen(n) {
-    var slider = document.getElementsByClassName('slider_men');
+
+function showDivs(n, slider, number) {
+    var slider = document.getElementsByClassName(slider);
     if (n > slider.length) {
         slideIndex = 1
     }
@@ -18,56 +18,13 @@ function showDivsMen(n) {
         slider[i].style.display = "none";
     }
     slider[slideIndex - 1].style.display = "flex";
-    document.getElementById('numberMen').innerHTML = slideIndex;
+    document.getElementById(number).innerHTML = slideIndex;
 }
 
-
-
-showDivsWomen(slideIndex);
-
-function plusDivsWomen(n) {
-    showDivsWomen(slideIndex += n);
-}
-
-function showDivsWomen(n) {
-    var slider = document.getElementsByClassName("slider_women");
-    if (n > slider.length) {
-        slideIndex = 1
-    }
-    if (n < 1) {
-        slideIndex = slider.length
-    };
-    for (let i = 0; i < slider.length; i++) {
-        slider[i].style.display = "none";
-    }
-    slider[slideIndex - 1].style.display = "flex";
-    document.getElementById('numberWomen').innerHTML = slideIndex;
-}
-
-showDivsChildren(slideIndex);
-
-function plusDivsChildren(n) {
-    showDivsChildren(slideIndex += n);
-}
-
-function showDivsChildren(n) {
-    var slider = document.getElementsByClassName("slider_children");
-    if (n > slider.length) {
-        slideIndex = 1
-    }
-    if (n < 1) {
-        slideIndex = slider.length
-    };
-    for (let i = 0; i < slider.length; i++) {
-        slider[i].style.display = "none";
-    }
-    slider[slideIndex - 1].style.display = "flex";
-    document.getElementById('numberChildren').innerHTML = slideIndex;
-}
 
 function showOrHide(sectionId, checkboxId) {
-    var checkbox = document.getElementById(checkboxId);
-    var section = document.getElementById(sectionId);
+    let checkbox = document.getElementById(checkboxId);
+    let section = document.getElementById(sectionId);
     if (checkbox.checked) {
         section.style.display = "block";
     } else {
@@ -76,8 +33,9 @@ function showOrHide(sectionId, checkboxId) {
 }
 
 function allShow() {
-var sections = document.getElementsByClassName('section_two');
+let sections = document.getElementsByClassName('section_two');
 for(let i=0; i<sections.length; i++){
     sections[i].style.display = "block";
 }
 }
+plusDivs(0,'sliderMen');
